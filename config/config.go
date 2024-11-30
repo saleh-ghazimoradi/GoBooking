7bbdba7fb17efd8c767cc48b3046d4b5e67547b2
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-11-30 21:51:17 +0330

    set configuration for the project using the godotenv package

---
+++ config/config.go
@@ -0,0 +1,55 @@
+package config
+
+import (
+	"github.com/caarlos0/env"
+	"github.com/joho/godotenv"
+	"log"
+)
+
+type Env struct {
+	Server     Server
+	Postgresql Postgresql
+}
+
+type Postgresql struct {
+	DbHost     string `env:"DB_HOST,required"`
+	DbPort     string `env:"DB_PORT,required"`
+	DbUser     string `env:"DB_USER,required"`
+	DbPassword string `env:"DB_PASSWORD,required"`
+	DbName     string `env:"DB_NAME,required"`
+	DbSslMode  string `env:"DB_SSLMODE,required"`
+}
+
+type Server struct {
+	Port string `env:"SERVER_PORT"`
+}
+
+func EnvConfig() *Env {
+	if err := godotenv.Load("app.env"); err != nil {
+		log.Fatalf("Unable to load app.env file: %e", err)
+	}
+
+	envConfig := &Env{}
+
+	if err := env.Parse(envConfig); err != nil {
+		log.Fatalf("Unable to parse app.env file: %e", err)
+	}
+
+	postConfig := &Postgresql{}
+
+	if err := env.Parse(postConfig); err != nil {
+		log.Fatalf("Unable to parse app.env file: %e", err)
+	}
+
+	envConfig.Postgresql = *postConfig
+
+	serverConfig := &Server{}
+
+	if err := env.Parse(serverConfig); err != nil {
+		log.Fatalf("Unable to parse app.env file: %e", err)
+	}
+
+	envConfig.Server = *serverConfig
+
+	return envConfig
+}
