c80785e5dc28b204b4d6f3667ef43d39a6c11367
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-12-01 20:19:42 +0330

    remove db.close from routes due to misplacement and add some routes to the registerRoutes function

--- internal/gateway/routes.go
+++ internal/gateway/routes.go
@@ -14,7 +14,6 @@ func registerRoutes() *httprouter.Router {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
 
 	eventRepository := repository.NewEventRepository(db, db)
 	eventService := service.NewEventService(eventRepository)
@@ -24,9 +23,11 @@ func registerRoutes() *httprouter.Router {
 	router.NotFound = http.HandlerFunc(notFoundRouter)
 	router.MethodNotAllowed = http.HandlerFunc(methodNotAllowedResponse)
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", healthCheckHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/events/:id", eventHandler.getOneHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/events", eventHandler.getManyHandler)
-	router.HandlerFunc(http.MethodPost, "/v1/events", eventHandler.createEventHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/events/:id", eventHandler.getOneEvent)
+	router.HandlerFunc(http.MethodGet, "/v1/events", eventHandler.getManyEvents)
+	router.HandlerFunc(http.MethodPost, "/v1/events", eventHandler.createEvent)
+	router.HandlerFunc(http.MethodDelete, "/v1/events/:id", eventHandler.deleteEvent)
+	router.HandlerFunc(http.MethodPut, "/v1/events/:id", eventHandler.updateEvent)
 
 	return router
 }
