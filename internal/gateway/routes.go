9115bddce6b8daafe43666b9426a6c58f586309b
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-12-01 12:36:09 +0330

    implement routing functionality for events

---
+++ internal/gateway/routes.go
@@ -0,0 +1,28 @@
+package gateway
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"github.com/saleh-ghazimoradi/GoBooking/internal/repository"
+	"github.com/saleh-ghazimoradi/GoBooking/internal/service"
+	"github.com/saleh-ghazimoradi/GoBooking/utils"
+	"log"
+	"net/http"
+)
+
+func registerRoutes() {
+	db, err := utils.PostConnection()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	eventRepository := repository.NewEventRepository(db, db)
+	eventService := service.NewEventService(eventRepository)
+	eventHandler := NewEventHandler(eventService)
+
+	router := httprouter.New()
+
+	router.HandlerFunc(http.MethodGet, "/v1/", eventHandler.getOneHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/", eventHandler.getManyHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/", eventHandler.createEventHandler)
+}
