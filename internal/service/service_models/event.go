516351c02e8d1bc3a5cef6437541bf563cb43b0d
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-12-01 20:22:22 +0330

    make improvements to the event models by add some tags

--- internal/service/service_models/event.go
+++ internal/service/service_models/event.go
@@ -3,10 +3,21 @@ package service_models
 import "time"
 
 type Event struct {
-	ID        int64
-	Name      string
-	Location  string
-	Date      time.Time
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID        int64     `json:"id"`
+	Name      string    `json:"name"`
+	Location  string    `json:"location"`
+	Date      time.Time `json:"date"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Version   int       `json:"version"`
+}
+
+type EventPayload struct {
+	Name     string `json:"name" validate:"required"`
+	Location string `json:"location" validate:"required"`
+}
+
+type UpdateEventPayload struct {
+	Name     *string `json:"name" validate:"required"`
+	Location *string `json:"location" validate:"required"`
 }
