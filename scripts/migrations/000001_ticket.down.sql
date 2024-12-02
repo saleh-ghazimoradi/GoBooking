44c7096f373e6008b9d8305def437927c0bf3683
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-12-02 21:20:10 +0330

    add script for ticket table

--- scripts/migrations/000001_ticket.down.sql
+++ scripts/migrations/000001_ticket.down.sql
@@ -1,1 +1,2 @@
 DROP TABLE IF EXISTS events;
+DROP TABLE IF EXISTS tickets;
--- scripts/migrations/000001_ticket.up.sql
+++ scripts/migrations/000001_ticket.up.sql
@@ -8,3 +8,12 @@ CREATE TABLE IF NOT EXISTS events (
 );
 
 ALTER TABLE events ADD COLUMN version INT DEFAULT 0;
+
+CREATE TABLE IF NOT EXISTS tickets (
+    id bigserial PRIMARY KEY,
+    event_id INT NOT NULL REFERENCES events(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    user_id INT NOT NULL,
+    entered BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP(0) with time zone NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP(0) with time zone NOT NULL DEFAULT NOW()
+);
