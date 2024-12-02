108d081726fd309b2aece76704488cdad54f48c8
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-12-02 21:23:23 +0330

    implement get, get many, and create functionality for the ticket in the repository layer

--- internal/repository/ticket.go
+++ internal/repository/ticket.go
@@ -3,11 +3,12 @@ package repository
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"github.com/saleh-ghazimoradi/GoBooking/internal/service/service_models"
 )
 
 type Ticket interface {
-	GetMany(ctx context.Context) ([]*service_models.Ticket, error)
+	GetMany(ctx context.Context, fq service_models.PaginationFeedQuery) ([]service_models.Ticket, error)
 	GetOne(ctx context.Context, id int64) (*service_models.Ticket, error)
 	CreateOne(ctx context.Context, ticket *service_models.Ticket) error
 	UpdateOne(ctx context.Context, ticket *service_models.Ticket) error
@@ -21,15 +22,66 @@ type ticketRepo struct {
 	tx      *sql.Tx
 }
 
-func (t *ticketRepo) GetMany(ctx context.Context) ([]*service_models.Ticket, error) {
-	return nil, nil
+func (t *ticketRepo) GetMany(ctx context.Context, fq service_models.PaginationFeedQuery) ([]service_models.Ticket, error) {
+	query := `SELECT id, event_id, entered, created_at, updated_at FROM tickets ORDER BY id ` + fq.Sort + ` LIMIT $1 OFFSET $2`
+
+	rows, err := t.dbRead.QueryContext(ctx, query, fq.Limit, fq.Offset)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var tickets []service_models.Ticket
+	for rows.Next() {
+		var ticket service_models.Ticket
+		err = rows.Scan(
+			&ticket.ID,
+			&ticket.EventID,
+			&ticket.Entered,
+			&ticket.CreatedAt,
+			&ticket.UpdatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		tickets = append(tickets, ticket)
+	}
+	return tickets, nil
 }
 
 func (t *ticketRepo) GetOne(ctx context.Context, id int64) (*service_models.Ticket, error) {
-	return nil, nil
+	query := `SELECT id, event_id, entered, created_at, updated_at FROM tickets WHERE id=$1`
+
+	var ticket service_models.Ticket
+	err := t.dbRead.QueryRowContext(ctx, query, id).Scan(
+		&ticket.ID,
+		&ticket.EventID,
+		&ticket.Entered,
+		&ticket.CreatedAt,
+		&ticket.UpdatedAt,
+	)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+	return &ticket, nil
+
 }
 
 func (t *ticketRepo) CreateOne(ctx context.Context, ticket *service_models.Ticket) error {
+	query := `INSERT INTO tickets (event_id, entered) VALUES ($1, $2) RETURNING id, created_at, updated_at`
+
+	err := t.dbWrite.QueryRowContext(ctx, query, ticket.EventID, ticket.Entered).Scan(&ticket.ID, &ticket.CreatedAt, &ticket.UpdatedAt)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
