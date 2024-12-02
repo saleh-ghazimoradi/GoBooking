318095902a1ca04dc08ad7c5c0e55da0fdf94491
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-12-02 21:21:05 +0330

    comment a field in the ticket model

--- internal/service/service_models/ticket.go
+++ internal/service/service_models/ticket.go
@@ -3,8 +3,9 @@ package service_models
 import "time"
 
 type Ticket struct {
-	ID        int64     `json:"id"`
-	EventID   int64     `json:"event_id"`
+	ID      int64 `json:"id"`
+	EventID int64 `json:"event_id"`
+	//UserID    int64     `json:"userId"`
 	Event     Event     `json:"event"`
 	Entered   bool      `json:"entered"`
 	CreatedAt time.Time `json:"created_at"`
@@ -13,4 +14,5 @@ type Ticket struct {
 
 type ValidateTicket struct {
 	TicketId int64 `json:"ticket_id"`
+	OwnerId  uint  `json:"ownerId"`
 }
